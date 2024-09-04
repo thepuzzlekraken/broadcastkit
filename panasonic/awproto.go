50faf968a7cba26789d30aba3c0ea3c508895a08
Zsolt Herczeg <herczegzsolt@puzzlekraken.com>
2024-09-04 20:23:14 +0200

    Improve AW notification proxying

--- panasonic/awproto.go
+++ panasonic/awproto.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -25,6 +26,8 @@ const (
 	awNty                          // expected over notifications interface
 	awHintCount awHint = 3
 )
+
+const networkTimeout = 3 * time.Second
 
 // AWResponse is the interface implemented by all responses sent from a camera.
 //
@@ -262,7 +265,7 @@ type CameraRemote struct {
 	// Some variables are adjusted for Panasonic protocol unless explicitly set
 	// before the first request is sent.
 	// CheckRedirects is set func(...) error { return http.ErrUseLastResponse }
-	// Timeout is set 1 * time.Second
+	// Timeout is set to 3 seconds
 	Http     http.Client
 	httpOnce sync.Once
 }
@@ -275,21 +278,24 @@ func (c *CameraRemote) httpInit() {
 		}
 	}
 	if c.Http.Timeout == 0 {
-		c.Http.Timeout = 1 * time.Second
+		c.Http.Timeout = networkTimeout
 	}
 }
 
 // httpGet does an http.Get to the camera with the quirks of the AW protocol
 func (c *CameraRemote) httpGet(path string, query string) (*http.Response, error) {
 	c.httpOnce.Do(c.httpInit)
+	// The AW-RP50 just makes a one-liner HTTP/1.0 request, then proceeds to
+	// provide a Host header anyway filled with an incorrectly zero-padded IP.
+	// 	 GET /cgi-bin/aw_ptz?cmd=#R00&res=1 HTTP/1.0
+	//   Host:198.051.100.008
+	// We use a proper HTTP client instead.
 	return c.Http.Do(&http.Request{
 		Method: "GET",
 		URL: &url.URL{
-			Scheme: "http",
-			Host:   c.Addr.String(),
-			Path:   path,
-			// Note that query is NOT urlencoded and will contain non-compliant
-			// characters like '#'. This matches the behavior of remote panels.
+			Scheme:   "http",
+			Host:     c.Addr.String(),
+			Path:     path,
 			RawQuery: query,
 		},
 		Proto:      "HTTP/1.1",
@@ -310,10 +316,11 @@ func (c *CameraRemote) strCommand(hint awHint, cmd string) (string, error) {
 	} else if hint&awCam != 0 {
 		path = "/cgi-bin/aw_cam"
 	} else {
-		return "", fmt.Errorf("this command %v")
+		return "", fmt.Errorf("unsupported command on AW interface: %v", cmd)
 	}
-
-	res, err := c.httpGet(path, "cmd="+cmd+"&res=1")
+	// Panasonic panels do NOT urlencode the command even though it contains #
+	// Since the specification permits encoding, we do it for http compliance.
+	res, err := c.httpGet(path, "cmd="+url.QueryEscape(cmd)+"&res=1")
 	if err != nil {
 		return "", err
 	}
@@ -356,12 +363,12 @@ func (c *CameraRemote) Command(req AWRequest) (AWResponse, error) {
 	return res, nil
 }
 
-func (c *CameraRemote) UpdateListener() (*CameraUpdateListener, error) {
+func (c *CameraRemote) NotificationListener() (*CameraNotifyListener, error) {
 	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{})
 	if err != nil {
 		return nil, &NetworkError{err}
 	}
-	return &CameraUpdateListener{
+	return &CameraNotifyListener{
 		lis: listener,
 		cam: c,
 	}, nil
@@ -406,57 +413,72 @@ func notifyUnpack(b []byte) (string, error) {
 // - 10 bytes constant, hex 00 01 00 80 00 00 00 00 00 01
 // -  2 bytes length, encoded as (l + 8) in uint16 big endian
 // -  4 bytes constant, hex 01 00 00 00
-// -  l bytes string data (surrounded by CRLF)
+// -  l bytes string data (surrounded by 2-2 bytes of CRLF)
 // -  4 bytes constant, hex 00 02 00 18
 // -  6 bytes source MAC
 // -  2 bytes constant, hex 00 01
 // -  6 bytes date, same as above
 // -  6 bytes constant 00 00 00 00 00 00
 // Date is in local time of the camera, not UTC.
-func notifyPack(response string, counter uint16, camIP netip.Addr, camMAC net.HardwareAddr, date time.Time) []byte {
-	// Surround the response with CRLF fluff
-	// TODO(zsh): Should we mimick the 0-padding quirks of the camera?
-	data := make([]byte, len(response)+4)
-	data[0] = '\r'
-	data[1] = '\n'
-	copy(data[2:], response)
-	data[len(data)-2] = '\r'
-	data[len(data)-1] = '\n'
+func notifyPack(response string, session *NotifySession, date time.Time) []byte {
+	offset := 30 + len(response)
+	length := offset + 26
 
-	// Create the data packet as described above
-	f := 4 + 2 + 6 + 10 + 2 + 4 + len(data) + 4 + 6 + 2 + 6 + 6
-	b := make([]byte, f)
-	_ = b[28]
-	copy(b, camIP.AsSlice())
+	b := make([]byte, length)
+	_ = b[offset+25]
+	_ = b[30]
+
+	srcip := session.srcIP
+	if srcip == (netip.Addr{}) {
+		// avoid panicing on uninitialized sessions
+		srcip = netip.IPv4Unspecified()
+	}
+	ipv4 := srcip.As4()
+	copy(b, ipv4[:])
+
+	counter := session.Counter.next()
 	binary.BigEndian.PutUint16(b[4:], counter)
+
 	b[6] = uint8(date.Year() % 100)
 	b[7] = uint8(date.Month())
 	b[8] = uint8(date.Day())
 	b[9] = uint8(date.Hour())
 	b[10] = uint8(date.Minute())
 	b[11] = uint8(date.Second())
-	copy(b[12:], []byte{0x00, 0x01, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01})
-	binary.BigEndian.PutUint16(b[22:], uint16(len(data)+8))
-	copy(b[24:], []byte{0x01, 0x00, 0x00, 0x00})
-	copy(b[28:], data)
-	copy(b[f-25:f-21], []byte{0x00, 0x02, 0x00, 0x18})
-	copy(b[f-21:f-15], camMAC)
-	copy(b[f-15:f-13], []byte{0x00, 0x01})
-	copy(b[f-13:f-7], b[6:12])
-	copy(b[f-7:f-1], []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+	copy(b[12:], "\x00\x01\x00\x80\x00\x00\x00\x00\x00\x01")
+
+	replen := len(response) + 4 + 8 // reported length includes CRLFs + 8 bytes
+	binary.BigEndian.PutUint16(b[22:], uint16(replen))
+
+	copy(b[24:], "\x01\x00\x00\x00\r\n")
+	copy(b[30:], response)
+	copy(b[offset:], "\r\n\x00\x02\x00\x18")
+
+	copy(b[offset+6:], session.srcMac)
+
+	copy(b[offset+12:], "\x00\x01")
+	copy(b[offset+14:], b[6:12]) // copy date instead of recalculating it
+	copy(b[offset+20:], "\x00\x00\x00\x00\x00\x00")
 
 	return b
 }
 
-type CameraUpdateListener struct {
+type CameraNotifyListener struct {
 	once sync.Once
 	lis  *net.TCPListener
 	cam  *CameraRemote
 }
 
-func (u *CameraUpdateListener) Start() error {
-	port := netip.MustParseAddrPort(u.lis.Addr().String()).Port()
-	res, err := u.cam.httpGet("/cgi-bin/event", "connect=start&my_port="+strconv.Itoa(int(port))+"&uid=0")
+func (l *CameraNotifyListener) Start() error {
+	err := l.start()
+	l.once.Do(func() {})
+	return err
+}
+
+func (l *CameraNotifyListener) start() error {
+	port := netip.MustParseAddrPort(l.lis.Addr().String()).Port()
+	res, err := l.cam.httpGet("/cgi-bin/event", "connect=start&my_port="+strconv.Itoa(int(port))+"&uid=0")
 	if err != nil {
 		return &NetworkError{err}
 	}
@@ -466,9 +488,9 @@ func (u *CameraUpdateListener) Start() error {
 	return nil
 }
 
-func (u *CameraUpdateListener) Stop() error {
-	port := netip.MustParseAddrPort(u.lis.Addr().String()).Port()
-	res, err := u.cam.httpGet("/cgi-bin/event", "connect=start&my_port="+strconv.Itoa(int(port))+"&uid=0")
+func (l *CameraNotifyListener) Stop() error {
+	port := netip.MustParseAddrPort(l.lis.Addr().String()).Port()
+	res, err := l.cam.httpGet("/cgi-bin/event", "connect=start&my_port="+strconv.Itoa(int(port))+"&uid=0")
 	if err != nil {
 		return &NetworkError{err}
 	}
@@ -478,14 +500,14 @@ func (u *CameraUpdateListener) Stop() error {
 	return nil
 }
 
-func (u *CameraUpdateListener) Addr() netip.AddrPort {
-	return netip.MustParseAddrPort(u.lis.Addr().String())
+func (l *CameraNotifyListener) Addr() netip.AddrPort {
+	return netip.MustParseAddrPort(l.lis.Addr().String())
 }
 
-func (u *CameraUpdateListener) acceptTCP() (*net.TCPConn, error) {
-	camaddr := u.cam.Addr
+func (l *CameraNotifyListener) acceptTCP() (*net.TCPConn, error) {
+	camaddr := l.cam.Addr
 	for {
-		conn, err := u.lis.AcceptTCP()
+		conn, err := l.lis.AcceptTCP()
 		if err != nil {
 			return nil, err
 		}
@@ -499,15 +521,15 @@ func (u *CameraUpdateListener) acceptTCP() (*net.TCPConn, error) {
 	}
 }
 
-func (u *CameraUpdateListener) Accept() (AWResponse, error) {
-	u.once.Do(func() { u.Start() })
+func (l *CameraNotifyListener) Accept() (AWResponse, error) {
+	l.once.Do(func() { l.start() })
 
-	conn, err := u.acceptTCP()
+	conn, err := l.acceptTCP()
 	if err != nil {
 		return nil, &NetworkError{err}
 	}
 
-	conn.SetDeadline(time.Now().Add(1 * time.Second))
+	conn.SetDeadline(time.Now().Add(networkTimeout))
 	b, err := io.ReadAll(conn)
 	conn.Close()
 
@@ -523,9 +545,180 @@ func (u *CameraUpdateListener) Accept() (AWResponse, error) {
 	return newResponse(awNty, cmd), nil
 }
 
-func (u *CameraUpdateListener) Close() error {
-	_ = u.Stop()
-	return u.lis.Close()
+func (l *CameraNotifyListener) Close() error {
+	_ = l.Stop()
+	return l.lis.Close()
+}
+
+func sendTCP(b []byte, dst netip.AddrPort) error {
+	conn, err := net.DialTimeout("tcp4", dst.String(), networkTimeout)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(networkTimeout))
+	_, err = conn.Write(b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// discoverMac returns the MAC address which would be used on the local network
+// with the given source IP. This may differ from the actual src MAC if the
+// packet is going through a gateway.
+func discoverMac(ip netip.Addr) net.HardwareAddr {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			panic(err)
+		}
+		for _, a := range addrs {
+			t := netip.MustParsePrefix(a.String())
+			if t.Contains(ip) {
+				return i.HardwareAddr
+			}
+		}
+	}
+	return nil
+}
+
+// discoverSource returns the IP address that would be autoselected for outgoing
+// connections to the provided destination.
+func discoverSource(dst netip.Addr) netip.Addr {
+	// Dialing incorrectly is not the most elegant thing to do, but we can avoid
+	// sending actual packets by using UDP instead of TCP for address discovery.
+	c, err := net.DialUDP("udp4", nil, net.UDPAddrFromAddrPort(
+		netip.AddrPortFrom(dst, 1),
+	))
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+	return netip.MustParseAddrPort(c.LocalAddr().String()).Addr()
+}
+
+// NotifyCounter is a 16-bit unsigned thread-safe counter for keeping track of
+// notification packet numbers.
+//
+// Implementation uses atomic.Uint32 as there's no atomic.Uint16. Applications
+// should never access the underlying value directly.
+type NotifyCounter atomic.Uint32
+
+// next yields the counter value for the next notification packet
+func (c *NotifyCounter) next() uint16 {
+	return uint16((*atomic.Uint32)(c).Add(1))
+}
+
+// Load returns the counter value of the last notification packet
+func (c *NotifyCounter) Load() uint16 {
+	return uint16((*atomic.Uint32)(c).Load())
+}
+
+// NotifySession is the metadata for the session
+//
+// Peer must not be modified after creation. Counter and Errors must be accessed
+// atomically. NotifySession contains unexported autodiscovered metadata.
+//
+// Use NewNotifySession to create a NotifySession with autodiscovery.
+type NotifySession struct {
+	Peer netip.AddrPort
+	// Figuring out the local source IP and MAC address is suprisingly complex.
+	// We store them as session data to avoid re-discovering at every packet.
+	srcIP   netip.Addr
+	srcMac  net.HardwareAddr
+	Counter NotifyCounter
+	Errors  atomic.Int32
+}
+
+// NewNotifySession creates a new NotifySession with autodiscovered metadata.
+func NewNotifySession(peer netip.AddrPort) *NotifySession {
+	srcIP := discoverSource(peer.Addr())
+	srcMac := discoverMac(srcIP)
+	return &NotifySession{
+		Peer:   peer,
+		srcIP:  srcIP,
+		srcMac: srcMac,
+	}
+}
+
+// Send sends a notification packet to the session.
+//
+// It retries up to 3 times and returns the error of the last try.
+func (s *NotifySession) Send(res AWResponse) error {
+	b := notifyPack(res.packResponse(), s, time.Now())
+	var err error
+	for i := 0; i < 3; i++ {
+		err = sendTCP(b, s.Peer)
+		if err == nil {
+			s.Errors.Store(0)
+			return nil
+		}
+		s.Errors.Add(1)
+		// Sleeping just 100ms is the backoff strategy of Panasonic hardware.
+		time.Sleep(100 * time.Millisecond)
+	}
+	return &NetworkError{err}
+}
+
+// NotifyList is a thread-safe locked list of NotifySessions
+type NotifyList struct {
+	lock sync.Mutex
+	// It has been observed that Panasonic allows only one notification session
+	// per IP address, stopping any previous ones when a new port is registered,
+	// effectively keying the map on IP instead of IP:PORT. Uncertain if it is
+	// intentional, we stick to IP:PORT mapping.
+	list map[netip.AddrPort]*NotifySession
+}
+
+// SendAll sends a notification to all active sessions.
+//
+// Sessions which have observed 3 or more errors (equal to one dropped message)
+// are automatically removed from the active sessions list.
+func (l *NotifyList) SendAll(res AWResponse) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	for dst, s := range l.list {
+		if s.Errors.Load() > 2 {
+			delete(l.list, dst)
+			continue
+		}
+		s.Send(res)
+	}
+}
+
+// Add adds a new peer to the session list
+func (l *NotifyList) Add(peer netip.AddrPort) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list == nil {
+		l.list = make(map[netip.AddrPort]*NotifySession)
+	}
+	l.list[peer] = NewNotifySession(peer)
+}
+
+// Remove removes a peer from the session list
+func (l *NotifyList) Remove(peer netip.AddrPort) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	delete(l.list, peer)
+}
+
+// Len returns the number of active sessions
+func (l *NotifyList) Len() int {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	for dst, s := range l.list {
+		if s.Errors.Load() > 2 {
+			delete(l.list, dst)
+			continue
+		}
+	}
+	return len(l.list)
 }
 
 type Handler interface {
@@ -538,40 +731,27 @@ func (f HandlerFunc) ServeAW(r AWRequest) AWResponse {
 	return f(r)
 }
 
+// HttpHandler is an http.Handler that implements an endpoint for AW protocol.
 type HttpHandler struct {
 	once      sync.Once
 	mux       http.ServeMux
 	AWHandler Handler
+	Sessions  NotifyList
 }
 
 func (c *HttpHandler) setup() {
 	c.mux = http.ServeMux{}
 	c.mux.HandleFunc("/cgi-bin/aw_ptz", c.servePtz)
 	c.mux.HandleFunc("/cgi-bin/aw_cam", c.serveCam)
-	c.mux.HandleFunc("/cgi-bin/man_session", func(w http.ResponseWriter, r *http.Request) {
-		// Quick workaround to make AW-RP50 think it is connected to us.
-		w.Write([]byte("Event session:1"))
-	})
+	c.mux.HandleFunc("/cgi-bin/event", c.serveEvent)
+	c.mux.HandleFunc("/cgi-bin/man_session", c.serveManSession)
 }
 
 func (c *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c.once.Do(c.setup)
-	c.mux.ServeHTTP(w, r)
-}
-
-func (c *HttpHandler) servePtz(w http.ResponseWriter, r *http.Request) {
-	c.serveAW(awPtz, w, r)
-}
-
-func (c *HttpHandler) serveCam(w http.ResponseWriter, r *http.Request) {
-	c.serveAW(awCam, w, r)
-}
-
-func (c *HttpHandler) serveAW(hint awHint, w http.ResponseWriter, r *http.Request) {
 	// There's no documentation about the error conditions that happen when
-	// the AW Protocol is violated. We mostly follow the spec instead, returning
-	// 400 Bad Request for anything wildly wrong with the request and 500 for
-	// anything wildly wrong inside the application.
+	// the AW Protocol is violated. We mostly follow the http spec instead,
+	// returning 400/500 for anything wrong with the request and the server
+	// respectively.
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Printf("panic: %v\n", r)
@@ -579,15 +759,30 @@ func (c *HttpHandler) serveAW(hint awHint, w http.ResponseWriter, r *http.Reques
 			return
 		}
 	}()
+	// There are some undocumented APIs behind other HTTP methods in real
+	// devices. We opt to not support them and return a proper 405 instead.
 	if r.Method != http.MethodGet {
-		// This is not what happens for real hardware, but we don't handle the
-		// undocumented xml interface that appears to be behind POST/PUT
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	// The spec appendix insists on all responses having no-cache directives.
+	w.Header().Add("Cache-Control", "no-cache")
+	c.once.Do(c.setup)
+	c.mux.ServeHTTP(w, r)
+}
+
+func (c *HttpHandler) servePtz(w http.ResponseWriter, r *http.Request) {
+	c.wrapAW(awPtz, w, r)
+}
+
+func (c *HttpHandler) serveCam(w http.ResponseWriter, r *http.Request) {
+	c.wrapAW(awCam, w, r)
+}
+
+func (c *HttpHandler) wrapAW(hint awHint, w http.ResponseWriter, r *http.Request) {
 	// Generate a "Bad Request" for missing parameters
 	qry := r.URL.Query()
-	if r := qry.Get("res"); r != "1" {
+	if qry.Get("res") != "1" {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
@@ -602,15 +797,49 @@ func (c *HttpHandler) serveAW(hint awHint, w http.ResponseWriter, r *http.Reques
 	w.Write([]byte(awres.packResponse()))
 }
 
-type notifyWatcher struct {
-	sequence uint16
-	sIP      netip.Addr
-	sMAC     net.HardwareAddr
+func (c *HttpHandler) serveEvent(w http.ResponseWriter, r *http.Request) {
+	// If the connect or my_port values are bad, we will respond with 400
+	// This does not match the Panasonic HW behavior, but we hate silent errors.
+	qry := r.URL.Query()
+	connect := qry.Get("connect")
+	port := qry.Get("my_port")
+	if (connect != "start" && connect != "stop") || port == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	portNo, err := strconv.ParseUint(port, 10, 16)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	// There's also a uid parameter, which seem to be ignored by cameras.
+	// Documentation states to set it to 0. AW-RP50 sets it to 50. We ignore it.
+
+	ip := netip.MustParseAddrPort(r.RemoteAddr).Addr()
+	client := netip.AddrPortFrom(ip, uint16(portNo))
+
+	switch connect {
+	case "start":
+		c.Sessions.Add(client)
+	case "stop":
+		c.Sessions.Remove(client)
+	default:
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
-// CameraEndpoint represent an incoming endpoint for AW commands
-type CameraEndpoint struct {
-	AWHandler   Handler
-	HttpHandler http.Handler
-	watchers    map[netip.AddrPort]notifyWatcher
+func (c *HttpHandler) serveManSession(w http.ResponseWriter, r *http.Request) {
+	// The command=get parameter is required. We opt to provide a 400 instead of
+	// the random mix of 204/403 codes observed in real devices.
+	if r.URL.Query().Get("command") != "get" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	// Quick workaround to make AW-RP50 think it is connected to us.
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Event session:"))
+	w.Write([]byte(strconv.Itoa(c.Sessions.Len())))
 }
